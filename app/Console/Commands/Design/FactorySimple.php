5c1753303e9bf7dbaa0a9d7b98c6261e7f3f26df
shalaaoao <605536038@qq.com>
2023-03-05 15:59:49 +0800

    简单工厂优化发送

--- app/Console/Commands/Design/FactorySimple.php
+++ app/Console/Commands/Design/FactorySimple.php
@@ -42,90 +42,95 @@ class FactorySimple extends Command
 
     public function handle()
     {
-        // 2B青年写法
-
-
         // 当前业务需要使用极光
-        echo $message = MessageFactory::createFactory(self::TYPE_ALI)
-                                      ->send('您又新的短消息，请查收~'), PHP_EOL;
+        echo MessageFactory::createFactory(self::TYPE_ALI, $userId = 1)
+                           ->send('您又新的短消息，请查收~'), PHP_EOL;
     }
 }
-
-// 普通2B青年写法
-class SendCommon2B{
-
-    public static function send(int $type, array $data)
-    {
-        switch ($type) {
-            case FactorySimple::TYPE_ALI:
-
-                // 对接阿里云，各种配置加载...
-                // curl
-
-            case FactorySimple::TYPE_BAIDU:
-
-                // 同上，对接百度云
-            case FactorySimple::TYPE_JIGUANG:
-
-                // 同上，对接极光
-
-            default:
-                return null;
-        }
-    }
-}
-
 
 interface FactorySimpleMessage
 {
     public function send(string $msg);
+
+    public function appPush();
 }
 
 class AliYunMessage implements FactorySimpleMessage
 {
+    public function __construct()
+    {
+        // 加载基础配置
+    }
+
+    public function setAAAA(int $userId)
+    {
+        // 根据参数加载一些特殊的配置
+        return $this;
+    }
+
     public function send(string $msg)
     {
         // 调用接口，发送短信
         // xxxxx
         return '阿里云短信（原阿里大鱼）发送成功！短信内容：' . $msg;
     }
+
+    public function appPush()
+    {
+
+    }
 }
 
 class BaiduYunMessage implements FactorySimpleMessage
 {
+    // 同上加载配置
+
     public function send(string $msg)
     {
         // 调用接口，发送短信
         // xxxxx
         return '百度SMS短信发送成功！短信内容：' . $msg;
     }
+
+    public function appPush()
+    {
+
+    }
 }
 
 class JiguangMessage implements FactorySimpleMessage
 {
+    // 同上加载配置
+
+
     public function send(string $msg)
     {
         // 调用接口，发送短信
         // xxxxx
         return '极光短信发送成功！短信内容：' . $msg;
     }
+
+    public function appPush()
+    {
+
+    }
 }
 
 class MessageFactory
 {
-    public static function createFactory($type)
+    public static function createFactory($type, int $userId)
     {
         // 极端情况下，这里创建对象会比较困难。
         switch ($type) {
             case FactorySimple::TYPE_ALI:
 
-                // 比如这里需要setAAAA
-                return new AliYunMessage();
+                return (new AliYunMessage())->setAAAA($userId);
             case FactorySimple::TYPE_BAIDU:
 
                 // 比如这里需要setBBBB
                 return new BaiduYunMessage();
             case FactorySimple::TYPE_JIGUANG:
+
                 return new JiguangMessage();
             default:
                 return null;
